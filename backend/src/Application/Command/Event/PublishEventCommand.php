e5a924efeabda877fe865fbbd72a51fb56f9f424
Mikolaj <mikoajmicht@gmail.com>
2025-10-24 16:14:44 +0200

    refactor(cleanup): remove dead code (16) [DTOs/CQRS/messaging/presenters/state]

--- backend/src/Application/Command/Event/PublishEventCommand.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace App\Application\Command\Event;
-
-final class PublishEventCommand
-{
-    public function __construct(public string $id, public string $userId) {}
-}
--- backend/src/Application/Query/Event/GetEventDetailsQuery.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace App\Application\Query\Event;
-
-final class GetEventDetailsQuery
-{
-    public function __construct(public string $id) {}
-}
